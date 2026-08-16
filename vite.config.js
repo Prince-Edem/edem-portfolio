1665d0f788bf695ce2ba743d5c0efa86d7f19468
Prince Edem Kobla Kutortse <princekoby4@gmail.com>
2026-08-16 11:20:44 +0000

    deployment fixes

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: "/edem-portfolio"
+  plugins: [react(), tailwindcss()]
 })
